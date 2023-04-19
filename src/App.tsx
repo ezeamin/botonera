094d04a104cb5f81ae4cbffea1d1280bdf4c98cc
Ezequiel Amin <ezequielamin@outlook.com>
2023-04-19 07:01:38 -0300

    improved buttons

--- src/App.tsx
+++ src/App.tsx
@@ -37,8 +37,8 @@ const App = () => {
       <h1 className='text-center'>Paaaase!</h1>
       <section className='row'>
         {buttons.map((button) => (
-          <article className='col-6 p-2'>
-            <MusicButton key={button.text} {...button} />
+          <article className='col-6 p-2' key={button.text}>
+            <MusicButton {...button} />
           </article>
         ))}
       </section>
